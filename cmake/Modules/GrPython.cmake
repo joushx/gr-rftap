d603fc33156580f533b97ca44d481725f9d63cab
Johannes Mittendorfer <joushx@users.noreply.github.com>
2021-01-16 10:41:20 +0100

    Python3 array index fix

--- cmake/Modules/GrPython.cmake
+++ cmake/Modules/GrPython.cmake
@@ -235,7 +235,7 @@ endfunction(GR_PYTHON_INSTALL)
 file(WRITE ${CMAKE_BINARY_DIR}/python_compile_helper.py "
 import sys, py_compile
 files = sys.argv[1:]
-srcs, gens = files[:len(files)/2], files[len(files)/2:]
+srcs, gens = files[:int(len(files)/2)], files[int(len(files)/2):]
 for src, gen in zip(srcs, gens):
     py_compile.compile(file=src, cfile=gen, doraise=True)
 ")
